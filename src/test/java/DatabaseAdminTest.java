35bd75377eea663e5c05a33b996fd4bf2ad285af
Chris Murphy <_chrism_@live.com>
2018-03-01 17:03:37 +0000

    tested all methods for databaseAdmin class.

--- src/test/java/DatabaseAdminTest.java
+++ src/test/java/DatabaseAdminTest.java
@@ -18,5 +18,40 @@ public class DatabaseAdminTest {
     }
 
     @Test
+    public void canChangeName(){
+        databaseAdmin.setName("Ulrich");
+        assertEquals("Ulrich", databaseAdmin.getName());
+    }
 
+    @Test
+    public void hasNI(){
+        assertEquals("12345", databaseAdmin.getNI());
+    }
+
+    @Test
+    public void canChangeNI(){
+        databaseAdmin.setNI("2468");
+        assertEquals("2468", databaseAdmin.getNI());
+    }
+
+    @Test
+    public void hasSalary(){
+        assertEquals(25000, databaseAdmin.getSalary());
+    }
+
+    @Test
+    public void canChangeSalary(){
+        databaseAdmin.setSalary(26000);
+        assertEquals(26000, databaseAdmin.getSalary());
+    }
+
+    @Test
+    public void canGetOnePercentOfSalary(){
+        assertEquals(250, databaseAdmin.raiseSalary(), 0.01);
+    }
+
+    @Test
+    public void canPayBonus(){
+        assertEquals(25250, databaseAdmin.payBonus(),0.01);
+    }
 }
